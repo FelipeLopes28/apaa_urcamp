f3ef280907b6e86c1d5e5632eba13a1340cbadd6
FelipeLopes28 <felipelopes182640@sou.urcamp.edu.br>
2020-11-05 09:45:32 -0300

    Update top bar

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,54 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import imgEmail from '../../assets/images/email.svg';
-import imgFacebook from '../../assets/images/facebook.svg';
-import imgInstagram from '../../assets/images/instagram.svg';
-import imgLetras from '../../assets/images/letras.svg';
-import logoSlogan from '../../assets/images/slogan_home.png'
+import imgEmail from "../../assets/images/email.svg";
+import imgFacebook from "../../assets/images/facebook.svg";
+import imgInstagram from "../../assets/images/instagram.svg";
+import imgLetras from "../../assets/images/letras.svg";
+import logoSlogan from "../../assets/images/slogan_home.png";
 
-import './styles.css';
+import "./styles.css";
 
 function PageHeader() {
   return (
     <header className="page-header">
-          <div className="top-bar-container">
-            <Link to="/contact">
-                <img src={imgEmail} alt="Contato"/>
-                apaadp@gmail.com
-            </Link>
-            <a target="_blank" 
-               rel ="noopener noreferrer" 
-               href="https://www.facebook.com/apaa.dp">
-               <img src={imgFacebook} alt="Facebook" className="face"/>
-            </a>
-            <a target="_blank" 
-               rel ="noopener noreferrer" 
-               href="https://www.instagram.com/apaadp/">
-               <img src={imgInstagram} alt="Instagram" className="insta"/>
-            </a>
-          </div>
+      <div className="top-bar-container">
+        <Link to="/contact">
+          <img src={imgEmail} alt="Contato" />
+          apaadp@gmail.com
+        </Link>
+        <div className="social-networks">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/apaa.dp">
+            <img src={imgFacebook} alt="Facebook" className="face" />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.instagram.com/apaadp/">
+            <img src={imgInstagram} alt="Instagram" className="insta" />
+          </a>
+        </div>
+      </div>
 
-          <div className="header-content">
-            <strong>
-                <img src={imgLetras} alt="Letreiro" className="letreiro"/>
-                <img src={logoSlogan} alt="Logo" className="logo"/>
-            </strong>
-
-          </div>
-          <div className="buttons-container">
-              <Link to="/home" className="home">
-                Home
-              </Link>
-              <Link to="/events" className="events">
-                Eventos
-              </Link>
-              <Link to="/contact" className="contact">
-                Contato
-              </Link>
-          </div>       
+      <div className="header-content">
+        <strong>
+          <img src={imgLetras} alt="Letreiro" className="letreiro" />
+          <img src={logoSlogan} alt="Logo" className="logo" />
+        </strong>
+      </div>
+      <div className="buttons-container">
+        <Link to="/home" className="home">
+          Home
+        </Link>
+        <Link to="/events" className="events">
+          Eventos
+        </Link>
+        <Link to="/contact" className="contact">
+          Contato
+        </Link>
+      </div>
     </header>
-
   );
 }
 

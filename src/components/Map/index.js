df38fa6370074b30219d92d15dd1d066d94af824
FelipeLopes28 <felipelopes182640@sou.urcamp.edu.br>
2020-11-09 12:39:56 -0300

    Update index.html

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,32 +1,36 @@
 import React from 'react';
-import {Container} from './styles';
+import { Container } from './styles';
 
- function Map() {
- 
+function Map() {
+
   return (
     <Container>
-         <div className="address">
-           <h4>Endereço:</h4>
-              <p>
-                Rua Moreira César, 1046, Prédio do INSS,  
-                segundo piso - Centro. 
-                Dom Pedrito - RS, 96450-000.
+      <div className="address">
+        <h4>Endereço:</h4>
+        <p>
+          Rua Moreira César, 1046, Prédio do INSS,
+          segundo piso - Centro.
+          Dom Pedrito - RS, 96450-000.
               </p>
-         </div>
-         <div className="telephone">
-           <h4>Telefone:</h4>
-            <p>
-              (53) 9.9910.8065
+      </div>
+      <div className="telephone">
+        <h4>Telefone:</h4>
+        <p>
+          (53) 9.9910.8065
             </p>
-         </div>
-         <div className="opening_hours">
-           <h4>Horário de atendimento:</h4>
-            <p>
-              Segundas, quartas e sextas
-              das 14h às 17h.
+      </div>
+      <div className="opening_hours">
+        <h4>Horário de atendimento:</h4>
+        <p>
+          Segundas, quartas e sextas
+          das 14h às 17h.
             </p>
-         </div>
-         
+      </div>
+
+      {/* <div className="map">
+        <h4>Mapa aqui</h4>
+      </div> */}
+
     </Container>
   );
 }

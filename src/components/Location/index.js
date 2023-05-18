d590067ee873dadcfb733d607d850a072c4c4775
FelipeLopes <45183852+FelipeLopes28@users.noreply.github.com>
2023-05-18 16:23:59 -0300

    Update index.js

--- src/components/Location/index.js
+++ src/components/Location/index.js
@@ -11,8 +11,7 @@ function Location() {
         <div className="address">
           <h4>Endereço:</h4>
           <p>
-            Rua Moreira César, 1046, Prédio do INSS,
-            segundo piso - Centro.
+            Andrade Neves, 1724, centro.
             Dom Pedrito - RS, 96450-000.
           </p>
         </div>
@@ -27,8 +26,8 @@ function Location() {
         <div className="opening_hours">
           <h4>Horário de atendimento:</h4>
           <p>
-            Segundas, quartas e sextas
-            das 14h às 17h.
+            De Segunda a Sexta-feira
+            das 08h às 12h e das 13h às 17h.
           </p>
         </div>
       </div>
